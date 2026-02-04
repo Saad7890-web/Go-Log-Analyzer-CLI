deeab1df2a3d1753dc6dc4546bb9319c162c563f
Saad7890-web <omysaadislam@gmail.com>
2026-02-04 22:55:03 +0600

    added Log Parser

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -1,1 +1,36 @@
 package parser
+
+import (
+	"strconv"
+	"strings"
+)
+
+type LogEntry struct {
+	IP string
+	Method string
+	Endpoint string
+	StatusCode int
+	ResponseTime int 
+}
+
+func ParseLine(line string)(LogEntry, error){
+	parts := strings.Fields(line)
+
+	status, err := strconv.Atoi(parts[3])
+	if err != nil {
+		return LogEntry{}, err
+	}
+
+	time,err:= strconv.Atoi(parts[4])
+	if err != nil {
+		return LogEntry{}, err
+	}
+
+	return LogEntry{
+		IP: parts[0],
+		Method: parts[1],
+		Endpoint: parts[2],
+		StatusCode: status,
+		ResponseTime: time,
+	}, nil
+}
